595d11f9bbe2af41d6861fbf131e4260efc221cf
robin0822 <125346665@qq.com>
2023-02-22 17:04:46 +0800

    revert

--- redis-test/index.js
+++ redis-test/index.js
@@ -2,7 +2,11 @@
  * @Author: robin0822 125346665@qq.com
  * @Date: 2023-02-22 13:45:56
  * @LastEditors: robin0822 125346665@qq.com
- * @LastEditTime: 2023-02-22 16:54:59
+<<<<<<< HEAD
+ * @LastEditTime: 2023-02-22 17:02:27
+=======
+ * @LastEditTime: 2023-02-22 14:23:13
+>>>>>>> parent of 25de09d (增加注释)
  * @FilePath: /node-blog/redis-test/index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -37,9 +41,8 @@ redisClient.connect().then(()=>{
 });
 
 
-console.log('test1111');
+
 
 console.log('test2222');
-
 
 
