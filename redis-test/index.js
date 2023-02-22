28f3cc20f6bb0423be783450dca62e609d1602e9
robin0822 <125346665@qq.com>
2023-02-22 16:55:19 +0800

    增加注释2

--- redis-test/index.js
+++ redis-test/index.js
@@ -2,7 +2,7 @@
  * @Author: robin0822 125346665@qq.com
  * @Date: 2023-02-22 13:45:56
  * @LastEditors: robin0822 125346665@qq.com
- * @LastEditTime: 2023-02-22 16:36:09
+ * @LastEditTime: 2023-02-22 16:54:59
  * @FilePath: /node-blog/redis-test/index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -38,6 +38,8 @@ redisClient.connect().then(()=>{
 
 
 console.log('test1111');
+
+console.log('test2222');
 
 
 
