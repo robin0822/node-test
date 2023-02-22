1f80153c656f90663812ee402b77c7ea995d2b56
robin0822 <125346665@qq.com>
2023-02-22 17:12:03 +0800

    Revert "index1 index2"
    This reverts commit cfc296583006e3b2c4280bf303b63a2268743f14.

--- redis-test/index2.js
+++
@@ -1,9 +0,0 @@
-/*
- * @Author: robin0822 125346665@qq.com
- * @Date: 2023-02-22 17:08:18
- * @LastEditors: robin0822 125346665@qq.com
- * @LastEditTime: 2023-02-22 17:08:30
- * @FilePath: /redis-test/index2.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
-console.log('index2');
