ea15d11c3a4a1d0d1d9380297dab531dad665162
robin0822 <125346665@qq.com>
2023-02-22 17:09:46 +0800

    index3

---
+++ redis-test/index3.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: robin0822 125346665@qq.com
+ * @Date: 2023-02-22 17:09:19
+ * @LastEditors: robin0822 125346665@qq.com
+ * @LastEditTime: 2023-02-22 17:09:30
+ * @FilePath: /redis-test/index3.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+console.log('index3');
